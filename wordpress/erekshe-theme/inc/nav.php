65eb53836a460a5ba498c8635b31c30a9a799bc0
Farik <farik@Mac-mini-work-4.local>
2026-09-14 15:37:21 +0500

    Revert "Навигация: скрыт раздел «Документы» из шапки и футера"
    This reverts commit 11407213bca11ddca34ef5998b2dcc06fa42f40e.

--- wordpress/erekshe-theme/inc/nav.php
+++ wordpress/erekshe-theme/inc/nav.php
@@ -16,6 +16,7 @@ function erekshe_nav_items() {
         ['slug' => 'team',        'key' => 'navTeam'],
         ['slug' => 'umay',        'key' => 'navUmay'],
         ['slug' => 'charity',     'key' => 'navCharity'],
+        ['slug' => 'documents',   'key' => 'navDocuments'],
         ['slug' => 'news',        'key' => 'navNews'],
         ['slug' => 'reviews-faq', 'key' => 'navReviews'],
         ['slug' => 'contacts',    'key' => 'navContacts'],
